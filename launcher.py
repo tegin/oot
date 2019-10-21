625283b3dd8fdd70d2602cf0b9588eb4d3af4255
Enric Tobella <etobella@creublanca.es>
2019-10-21 16:58:14 +0100

    [FIX] change file name for data

--- launcher.py
+++ launcher.py
@@ -24,5 +24,5 @@ data_folder = path + "/data"
 if not os.path.isdir(data_folder):
     os.mkdir(data_folder)
 OddoorCB(
-    data_folder + "/data_iot.json", CardReader(spd=200), KeyPad(), Buzzer(7, 13)
+    data_folder + "/data.json", CardReader(spd=200), KeyPad(), Buzzer(7, 13)
 ).run()
