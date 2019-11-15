36d372939dc5bbe65eb7106746f747a87ea29a69
Enric Tobella <etobella@creublanca.es>
2019-11-15 12:36:21 +0000

    [FIX] is_interface_up

--- oot/server/utils.py
+++ oot/server/utils.py
@@ -2,5 +2,8 @@ import netifaces
 
 
 def is_interface_up(interface):
-    addr = netifaces.ifaddresses(interface)
+    try:
+        addr = netifaces.ifaddresses(interface)
+    except ValueError:
+        return False
     return addr.get(netifaces.AF_INET, False)
